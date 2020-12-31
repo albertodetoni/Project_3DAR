b1a8825255e0c913b2f678913e0e5969be3b40fd
albertodetoni <alberto.detoni.97@gmail.com>
2020-12-31 13:23:36 +0100

    A

--- Project.m
+++ Project.m
@@ -1,11 +1,18 @@
 clear all; close all; clc;
 
-imds = imageDatastore(...
-    'Camera_Calibration\Testing\tisoDataset');
+% imds = imageDatastore(...
+%     'Camera_Calibration\Training\portelloDataset', ...
+%     'Camera_Calibration\Training\castle-P19\images', ...
+%     'Camera_Calibration\Training\castle-P30\images');
+imdsTraining = imageDatastore(...
+    'Camera_Calibration\Training', 'IncludeSubfolders', 1);
+
+imdsTest = imageDatastore(...
+    'Camera_Calibration\Testing', 'IncludeSubfolders', 1);
 
 dim=250; % dimension all the images will have
-for i = 1:numel(imds.Files) % for each image
-    I = rgb2gray(readimage(imds, i)); % store and convert to gray the image
+for i = 1:numel(imdsTraining.Files) % for each image
+    I = rgb2gray(readimage(imdsTraining, i)); % store and convert to gray the image
     
     if size(I,1)>size(I,2) % padding the images to be all squares
         I = imresize(I, [dim NaN]);
@@ -25,9 +32,20 @@ for i = 1:numel(imds.Files) % for each image
     points{i} = detectSURFFeatures(images{i});
     
     imshow(images{i}); hold on;
-    plot(points{i}.selectStrongest(10)); hold off;  
+    plot(points{i}.selectStrongest(10)); hold off;
+    
+    autoenc = trainAutoencoder(points{i}.Location, 15);
     
 end
 
-HiddenLayerSize=50;
-autoenc = trainAutoencoder(images, HiddenLayerSize);
+HiddenLayerSize=15;
+%autoenc = trainAutoencoder(images, HiddenLayerSize);
+
+%%
+
+for i = 1:numel(imdsTest.Files)
+    I = rgb2gray(readimage(imdsTraining, i));
+    I=double(I);
+    
+    Y_test{i} = predict(autoenc, I);
+end

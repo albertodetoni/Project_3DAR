f6664ae3c5d76cced0a38c7ea6b45e0443c904c1
albertodetoni <alberto.detoni.97@gmail.com>
2021-01-02 18:53:10 +0100

    A

--- Project.m
+++ Project.m
@@ -9,22 +9,22 @@ imdsTraining = imageDatastore(...
 
 imdsTest = imageDatastore(...
     'Camera_Calibration\Testing', 'IncludeSubfolders', 1);
-
+h=figure;
 dim=250; % dimension all the images will have
 for i = 1:numel(imdsTraining.Files) % for each image
     I = rgb2gray(readimage(imdsTraining, i)); % store and convert to gray the image
     
-    if size(I,1)>size(I,2) % padding the images to be all squares
-        I = imresize(I, [dim NaN]);
-        longdim = size(I,2);
-        I = padarray(I, [0 floor((dim-longdim)/2)], 0, 'pre');
-        I = padarray(I, [0 ceil((dim-longdim)/2)], 0, 'post');
-    else
-        I = imresize(I, [NaN dim]);
-        longdim = size(I,1);
-        I = padarray(I, [floor((dim-longdim)/2) 0], 0, 'pre');
-        I = padarray(I, [ceil((dim-longdim)/2) 0], 0, 'post');
-    end
+%     if size(I,1)>size(I,2) % padding the images to be all squares
+%         I = imresize(I, [dim NaN]);
+%         longdim = size(I,2);
+%         I = padarray(I, [0 floor((dim-longdim)/2)], 0, 'pre');
+%         I = padarray(I, [0 ceil((dim-longdim)/2)], 0, 'post');
+%     else
+%         I = imresize(I, [NaN dim]);
+%         longdim = size(I,1);
+%         I = padarray(I, [floor((dim-longdim)/2) 0], 0, 'pre');
+%         I = padarray(I, [ceil((dim-longdim)/2) 0], 0, 'post');
+%     end
     
     
     images{i} = I;  %store images in the cell array
@@ -32,9 +32,10 @@ for i = 1:numel(imdsTraining.Files) % for each image
     points{i} = detectSURFFeatures(images{i});
     
     imshow(images{i}); hold on;
-    plot(points{i}.selectStrongest(10)); hold off;
+    plot(points{i}); hold off;
     
-    autoenc = trainAutoencoder(points{i}.Location, 15);
+%     autoenc = trainAutoencoder(points{i}.Location, 15);
+    autoenc = trainAutoencoder(points{i}, 15);
     
 end
 
